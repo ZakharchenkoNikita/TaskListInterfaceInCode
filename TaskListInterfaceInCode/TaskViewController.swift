3ebadabed4e67e1826f6b67323f6c47d2c37c570
immortalprogrammist <immortalvelial@icloud.com>
2021-10-23 12:33:41 +0200

    add setup SubViews

--- TaskListInterfaceInCode/TaskViewController.swift
+++ TaskListInterfaceInCode/TaskViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    private lazy var taskTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Add new task"
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setup(subViews: taskTextField)
+    }
+    
+    private func setup(subViews: UIView...) {
+        subViews.forEach { subView in
+            view.addSubview(subView)
+        }
     }
 }
