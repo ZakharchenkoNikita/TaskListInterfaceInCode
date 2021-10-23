431c6cc5bb7b78e3c3b20795705d73c711b6103d
immortalprogrammist <immortalvelial@icloud.com>
2021-10-23 15:01:19 +0200

    setup Table

--- TaskListInterfaceInCode/TaskListViewController.swift
+++ TaskListInterfaceInCode/TaskListViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class TaskListViewController: UITableViewController {
-
+    
+    private var taskList: [Task] = []
+    private let cellID = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -16,8 +19,27 @@ class TaskListViewController: UITableViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        
+        taskList = DataManager.shared.fetchData()
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.name
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    //MARK: Private Metrhods
     private func setupNavigationBar() {
         title = "Task List"
         navigationController?.navigationBar.prefersLargeTitles = true
